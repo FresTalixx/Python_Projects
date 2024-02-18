b8275bc97661f1b095ac7ffcfbd23e714d791972
FresTalixx <frestalixx2@gmail.com>
2024-02-18 20:21:01 +0200

    Added beta features

--- Homework1.py
+++ Homework1.py
@@ -2,7 +2,10 @@ print("Hello world")
 
 
 class Cat:
-    def __init__(self, name, hunger):
+    def __init__(self, name, hunger,size):
         self.name = name
         self.hunger = hunger
+        self.size = size
 
+    def grow(self):
+        self.size += 1

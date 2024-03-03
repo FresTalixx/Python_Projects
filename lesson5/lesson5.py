9c7a497bc60d33eb63a040140c4d2920b7bae4bc
FresTalixx <frestalixx2@gmail.com>
2024-03-03 10:33:57 +0200

    Added new classes functions

---
+++ lesson5/lesson5.py
@@ -0,0 +1,45 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return f"{self.name} щось каже"
+
+
+class Dog(Animal):  # Успадкування
+    def bark(self):
+        return f"{self.name} голосно гавкає"
+
+
+animal = Animal("Тварина")
+print(animal.speak())
+
+dog = Dog("Собака")
+print(dog.speak())
+print(dog.bark())
+
+
+class BreedDog(Animal):
+
+    def __init__(self, name, breed):
+        super().__init__(name)  # щоб не було конфліктів з name у класі Animal
+        self.breed = breed
+
+    def speak(self):
+        # Виклик speak() з класу Animal
+        return f"{super().speak()} і {self.name} є {self.breed}"  # Зразу 2 функції speak без помилки. Це є метод super
+
+
+breed_dog = BreedDog("Собака", "Дог")
+print(breed_dog.speak())
+
+
+class MyClass:
+    def __init__(self, value):
+        self.__my_private_def(value)
+
+    def __my_private_def(self, value):
+        print(f"Private method called with value: {value}")
+
+
+obj = MyClass(54)
